e2e4dd253cafdc2c41b8702ea0cae5b790cd723f
juanclosada <juan_closada@soy.sena.edu.co>
2025-06-24 21:43:08 -0500

    Se definen las funciones en la carpeta Config

--- config/config.php
+++ config/config.php
@@ -1,5 +1,10 @@
 <?php
 
-define('URL', 'localhost:8040/jack2/');
-define('VIEW_PATH', URL . 'Vista/');
-define('CONTROLLER_PATH', URL . 'Controlador/');
+define('VIEW_PATH', __DIR__ . '/../Vista/');
+define('ENCABEZADO', __DIR__ . '/../Vista/Encabezado.php');
+define('PIEPAGINA', __DIR__ . '/../Vista/Piepagina.php');
+define('CONTROLLER_PATH', __DIR__ . '/../Controlador/');
+define('IMG_PATH', 'jack2/Vista/img/');
+define('CSS_PATH', 'jack2/Vista/css/');
+define('JS_PATH', 'jack2/Vista/js/');
+define('LIB_PATH', 'jack2/Vista/lib/');
