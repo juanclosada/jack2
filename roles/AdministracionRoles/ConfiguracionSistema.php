6c660f147153cad3f0987064b249bf01fd8fc750
juanclosada <juan_closada@soy.sena.edu.co>
2025-06-30 23:47:26 -0500

    pruebas de roles

--- roles/AdministracionRoles/ConfiguracionSistema.php
+++ roles/AdministracionRoles/ConfiguracionSistema.php
@@ -1,8 +1,1 @@
-<?php
-session_start();
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $correo = $_POST['correo'];
-    $contrasena = $_POST['contrasena'];
-}
--- roles/dashboardadmin.php
+++ roles/dashboardadmin.php
@@ -1,24 +1,41 @@
+<?php
+session_start();
+require_once '../config/config.php';
+include  ENCABEZADO;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador</title>
-</head>
 
 <body>
+    <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Administrador</title>
+    </head>
+
+
     <h1>Bienvenido al panel de administración</h1>
-    <p>Hola, <?php session_start(); echo $_SESSION['usuario']; ?>. Eres un administrador.</p>
+    <p>Hola, 
+        <?php 
+
+         echo $_SESSION['nombre']; 
+         ?>. 
+         Eres un administrador.</p>
     <a href="../logout.php">Cerrar sesión</a>
 
     <h2>Opciones de administración</h2>
     <ul>
-        <li><a href="../minegocio/frontend/src/index.js">Gestionar usuarios</a></li>
-        <li><a href="ver_reportes.php">Ver reportes</a></li>
-        <li><a href="configuracion.php">Configuración del sistema</a></li>
+        <li><a href="../roles/AdministracionRoles/GestionarUsuarios.php">Gestionar usuarios</a></li>
+        <li><a href="../roles/AdministracionRoles/verreportes.php">Ver reportes</a></li>
+        <li><a href="../roles/AdministracionRoles/configuracion.php">Configuración del sistema</a></li>
     </ul>
 </body>
-    header("Location:../index.js");
-
+   
 </html>
+
+<?php
+   // include 'conexion.php';
+    include_once PIEPAGINA; // contiene el navbar
+?>
