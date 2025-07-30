22a3198053b4e941bfa94d8decf665dbda651a89
Leonardo Leon <andresleonardo2417@gmail.com>
2025-07-30 10:26:54 -0500

    corregir enlace en el encabezado de la página de inicio de sesión

--- Vista/login.php
+++ Vista/login.php
@@ -31,7 +31,7 @@ if (!empty($_SESSION['usuario'])) {
 <body>
     <div class="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
         <div class="col-lg-4">
-            <a href="" class="text-decoration-none">
+            <a href="../vista/index.php" class="text-decoration-none">
                 <span class="h1 text-uppercase text-primary bg-dark px-2">INDUSTRIA</span>
                 <span class="h1 text-uppercase text-dark bg-primary px-2 ml-n1">ALCOBAS</span>
                 <span class="h1 text-uppercase text-primary bg-dark px-2">2JACK</span>
